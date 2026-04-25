559a1c1c4e75b9625d125b9322995ede09f4b91f
Patrick Calahan <github@pcal.net>
2026-04-25 03:12:10 -0700

    checkpoint

---
+++ cli/emulators/ares/info.sh
@@ -0,0 +1,9 @@
+
+# Path to the emulator's support directory, relative to $HOME/Library/
+EMU_SUPPORT_PATH="Application Support/Bannister/Nestopia"
+
+# Direct path to the emulator application.
+EMU_APP_PATH="/Applications/ares.app"
+
+# Options passed to the emulator.
+EMU_OPTIONS="--fullscreen"
