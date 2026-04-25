2ed1c02d7d81016c6b7f2b207a9538911ad3a0d2
Patrick Calahan <github@pcal.net>
2026-04-25 16:18:41 -0700

    checkpoint

--- cli/subcommands/retroapp-icns.sh
+++ cli/subcommands/retroapp-icns.sh
@@ -24,11 +24,37 @@ if [ ! -d "$TMP_ICONSET" ]; then
   exit 1
 fi
 
+
 # Sizes needed for macOS iconset
 SIZES=(16 32 64 128 256 512 1024)
 
+# Crop input to centered square if needed
+WIDTH=$(sips -g pixelWidth "$INPUT" | awk '/pixelWidth:/ {print $2}')
+HEIGHT=$(sips -g pixelHeight "$INPUT" | awk '/pixelHeight:/ {print $2}')
+
+if [ "$WIDTH" -ne "$HEIGHT" ]; then
+  # Find crop size and offsets
+  if [ "$WIDTH" -gt "$HEIGHT" ]; then
+    CROP_SIZE="$HEIGHT"
+    CROP_X=$(( (WIDTH - HEIGHT) / 2 ))
+    CROP_Y=0
+  else
+    CROP_SIZE="$WIDTH"
+    CROP_X=0
+    CROP_Y=$(( (HEIGHT - WIDTH) / 2 ))
+  fi
+  CROP_INPUT="$TMP_DIR/cropped.png"
+  sips -c "$CROP_SIZE" "$CROP_SIZE" --cropOffset "$CROP_X" "$CROP_Y" "$INPUT" --out "$CROP_INPUT" >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Error cropping image to square" >&2
+    exit 1
+  fi
+else
+  CROP_INPUT="$INPUT"
+fi
+
 for size in $SIZES; do
-  sips  -s format png -z "$size" "$size" "$INPUT" --out "$TMP_ICONSET/icon_${size}x${size}.png" >/dev/null 2>&1
+  sips -s format png -z "$size" "$size" "$CROP_INPUT" --out "$TMP_ICONSET/icon_${size}x${size}.png" >/dev/null 2>&1
   if [ $? -ne 0 ]; then
     echo "Error resizing image to ${size}x${size}" >&2
     exit 1
