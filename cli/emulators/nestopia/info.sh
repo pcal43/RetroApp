562da2353ec414f1712e663a6b7eaf5ec7e2974d
Patrick Calahan <github@pcal.net>
2026-04-25 01:41:38 -0700

    checkpoint

---
+++ cli/emulators/nestopia/info.sh
@@ -0,0 +1,13 @@
+
+# Path to the emulator's support directory, relative to $HOME/Library/
+EMU_SUPPORT_PATH="Application Support/Bannister/Nestopia"
+
+# Label used in the sandboxed home dir path.  The launcher's HOME will be
+# ~/Library/Application Support/RetroApp/$EMU_SANDBOX_HOME/$GAME_NAME
+EMU_SANDBOX_HOME="Nestopia"
+
+# Direct path to the emulator application.
+EMU_APP_PATH="/Applications/Nestopia.app"
+
+# Options passed to the emulator.
+EMU_OPTIONS="-nogui -fastboot -fullscreen -batch"
