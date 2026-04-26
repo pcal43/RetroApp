00c824e630844279d5b87ac75c168ce4faefe00a
Patrick Calahan <github@pcal.net>
2026-04-25 20:41:28 -0700

    checkpoint

--- cli/emulators/dolphin/info.sh
+++ cli/emulators/dolphin/info.sh
@@ -2,7 +2,7 @@
 EMU_NAME='Dolphin'
 
 # URL presented to the user if they need to download the emulator
-EMU_DOWNLOAD_URL='https://ares-emu.net/download'
+EMU_DOWNLOAD_URL='https://dolphin-emu.org/download'
 
 # Direct path to the emulator application.
 EMU_APP_PATH='/Applications/ares.app'
